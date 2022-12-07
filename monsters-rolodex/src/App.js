615e04f629de5e971942eb85036db9afdfe60353
icecoffee98 <yixuanyu98@gmail.com>
2022-12-07 10:31:25 -0800

    monster finished

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,30 +1,25 @@
-import { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Component } from 'react';
+import logo from './logo.svg';
+import SearchBox from './components/search-box/search-box.component';
+import CardList from './components/card-list/card-list.component';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
-      searchField: "",
+      searchField: '',
     };
-    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log("componentDidMount");
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((mstrs) => {
-        this.setState(
-          () => {
-            return { monsters: mstrs };
-          },
-          () => {
-            console.log(this.state.monsters);
-          }
-        );
+        this.setState(() => {
+          return { monsters: mstrs };
+        });
       });
   }
 
@@ -34,7 +29,6 @@ class App extends Component {
    * everytime when 'onChange' is called, which is less performance
    */
   onSearchChange = (event) => {
-    console.log(event.target.value);
     const searchField = event.target.value.toLowerCase();
     this.setState(() => {
       return { searchField };
@@ -42,8 +36,6 @@ class App extends Component {
   };
 
   render() {
-    console.log("render");
-
     // use destruction in ES6 to avoid using too many 'this'
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
@@ -53,18 +45,14 @@ class App extends Component {
     );
 
     return (
-      <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={onSearchChange}
+      <div className='App'>
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox
+          className='monster-search-box'
+          placeholder='please input the name'
+          onChangeHandler={onSearchChange}
         />
-        {filteredMonsters.map((monster) => (
-          <div key={monster.id}>
-            <h2>{monster.name}</h2>
-          </div>
-        ))}
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
